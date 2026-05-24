da169fb2eab2df1896a345a6ad742c131c287b90
junaidr04 <jbj.jawad@gmail.com>
2026-05-24 20:15:18 +0600

    Ecommerce models

---
+++ 06_models_ecommerce/order.models.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+import { Product } from "./product.models";
+const orderIntemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    orderItems: {
+        type: [orderIntemSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PENDING", "CANCELLED", "DELIVERED"],
+        default: "PENDING"
+    }
+}, { timestamps: true })
+export const Order = mongoose.model('Order', orderSchema);
